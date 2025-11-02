94a0e2b88c59ee9af2a4e109bb6885a07a9de391
Tshepo Majoro <mothofeelama@gmail.com>
2025-11-02 19:42:52 +0200

    added another section to the main content of the page

--- bonsai_object_manipulation.js
+++ bonsai_object_manipulation.js
@@ -5,6 +5,10 @@ const periods = document.getElementsByClassName("period")
 
 const values = document.getElementsByClassName("value");
 
+const per_period = document.getElementsByClassName("per_period");
+
+const add_on_costs = document.getElementsByClassName("bold_text");
+
 checkbox.addEventListener("click", (event) => {
     if (event.target.checked) {
         promo.textContent = "17% off!";
@@ -16,6 +20,13 @@ checkbox.addEventListener("click", (event) => {
         values[0].textContent = ((24*12) - ((24*12) * 0.17)).toFixed(2);
         values[1].textContent = ((39*12) - ((39*12) * 0.17)).toFixed(2);
         values[2].textContent = ((79*12) - ((79*12) * 0.17)).toFixed(2);
+
+        per_period[0].textContent = "/YEAR";
+        per_period[1].textContent = "/YEAR";
+
+        add_on_costs[1].textContent = "$" + ((9*12) - ((9*12) * 0.17)).toFixed(2);
+        add_on_costs[2].textContent = "$" + ((10*12) - ((10*12) * 0.17)).toFixed(2);
+
     } else {
         promo.textContent = "2 MONTHS FREE!";
 
@@ -26,5 +37,12 @@ checkbox.addEventListener("click", (event) => {
         values[0].textContent = 24;
         values[1].textContent = 39;
         values[2].textContent = 79;
+
+        per_period[0].textContent = "/MONTH";
+        per_period[1].textContent = "/MONTH";
+
+        add_on_costs[1].textContent = "$9";
+        add_on_costs[2].textContent = "$10";
+        
     }
 });
